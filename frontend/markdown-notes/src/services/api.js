c22d72e2e6663d0f23bbd88a6317e8fae75cfaa0
kundharam sai vamshi <156309354+kundharamvamshi@users.noreply.github.com>
2026-04-15 00:07:10 +0530

    initial commit

--- frontend/markdown-notes/src/services/api.js
+++ frontend/markdown-notes/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/notes'
+    baseURL: 'https://markdownnotes.onrender.com/notes',
 }); 
 
 export default api;
